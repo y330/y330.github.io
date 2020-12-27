49b2a4f717de9037d9d73cac9ffecb8202ac3dda
Yonah Aviv <avivyonah@gmail.com>
2020-12-27 07:46:39 +0000

    other

--- src/js/app.js
+++ src/js/app.js
@@ -2,17 +2,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   const sweetScroll = new SweetScroll({/* some options */});
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-// window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()};
 
-// function scrollFunction() {
-//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-//     document.getElementById("navbar").style.padding = "30px 10px";
-//     document.getElementById("my_name").style.fontSize = "25px";
-//   } else {
-//     document.getElementById("navbar").style.padding = "80px 10px";
-//     document.getElementById("my_name").style.fontSize = "35px";
-//   }
-// }
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("my_name").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("my_name").style.fontSize = "35px";
+  }
+}
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
   particlesJS('particles-js', {
     "particles": {
